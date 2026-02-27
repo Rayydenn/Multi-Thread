9de6353afba78fbf9d735a69a0210bb4ef8b9820
Imad MELLOUKI FILALI <imad.melloukifilali@student.hepl.be>
2026-02-27 10:49:30 +0100

    Fin étapes

--- Exercice3/etape4.cpp
+++ Exercice3/etape4.cpp
@@ -22,17 +22,38 @@ DONNEE data[] = { "MATAGNE",15,
                   "",0 };
 
 void* threadsecond(void* p);
+void HandlerSIGINT(int sig);
+void LiberationCompteur(void* p);
 
 DONNEE Param;
 int compteur = 0;
 
 pthread_mutex_t mutexCompteur;
 pthread_cond_t condCompteur;
+pthread_key_t cle;
 
 int main() { 
     pthread_t thsec[4];
     int i, nbth = 0;
-    pthread_key_t cle;
+
+    struct sigaction A;
+
+    A.sa_handler = HandlerSIGINT;
+    sigemptyset(&A.sa_mask);
+    A.sa_flags = 0;
+
+    if ((sigaction(SIGINT, &A, NULL)) == -1)
+    {
+      perror("Erreur lors de l'armement de SIGINT\n");
+      exit(1);
+    }
+    printf("Le signal SIGINT à bien été armé \n");
+
+    sigset_t mask;
+    sigfillset(&mask);
+    pthread_sigmask(SIG_SETMASK, &mask, NULL);
+
+    pthread_key_create(&cle, LiberationCompteur);
 
     pthread_mutex_init(&mutexCompteur, NULL);
     pthread_cond_init(&condCompteur, NULL);
@@ -74,6 +95,16 @@ void* threadsecond(void* p) {
     parm = *(DONNEE*)p;
     pthread_mutex_unlock(&mutexCompteur);
 
+    sigset_t mask;
+    sigfillset(&mask);
+    sigdelset(&mask, SIGINT);
+    pthread_sigmask(SIG_SETMASK, &mask, NULL);
+
+    char *nomp = new char[strlen(parm.nom) + 1];
+    strcpy(nomp, parm.nom);
+
+    pthread_setspecific(cle, nomp);
+
     char txt[120];
     sprintf(txt, "Thread (%d,%p) %s demarre...\n",getpid(),pthread_self(), parm.nom); 
     TRACE(txt);
@@ -94,4 +125,20 @@ void* threadsecond(void* p) {
     pthread_mutex_unlock(&mutexCompteur);
 
     return NULL;
+}
+
+void LiberationCompteur(void* p)
+{
+    TRACE("Libération de ma variable spécifique Compteur.\n");
+    delete[] (char*) p;
+}
+
+void HandlerSIGINT(int sig)
+{
+    char *nom = (char*) pthread_getspecific(cle);
+
+    printf("\nThread %d.%lu s'occupe de \"%s\"\n",
+           getpid(),
+           pthread_self(),
+           nom);
 }
